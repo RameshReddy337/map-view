5d80b897ba2d0c609b42445254699f2acc1cb756
RameshReddy337 <reddys1273@gmail.com>
2024-07-30 18:13:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import MapView from './MapView';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapView />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MapView.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import { loadModules } from 'esri-loader';
+
+const MapView = () => {
+  const mapViewRef = useRef(null);
+  const layerRef = useRef(null);
+
+  useEffect(() => {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/widgets/Search'], { css: true })
+      .then(([Map, MapView, FeatureLayer, Search]) => {
+        const map = new Map({
+          basemap: 'topo-vector',
+        });
+
+        const view = new MapView({
+          container: 'mapDiv',
+          map: map,
+          // center: [12.972442, 77.580643],
+          zoom: 4,
+        });
+
+        const layer = new FeatureLayer({
+          url:
+            'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/origins/FeatureServer/0',
+        });
+
+        map.addMany([layer]);
+        layerRef.current = layer;
+
+        const searchWidget = new Search({
+          view: view,
+          sources: [],
+        });
+
+        view.ui.add(searchWidget, {
+          position: 'top-left',
+        });
+
+        mapViewRef.current = view;
+      })
+      .catch((error) => {
+        console.error('Error loading Esri modules:', error);
+      });
+  }, []);
+
+  return <div id="mapDiv" style={{ height: '100vh', width: '100vw' }} />;
+};
+
+export default MapView;
